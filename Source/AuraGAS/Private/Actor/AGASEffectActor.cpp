cde4ba901d5bb8dbf4f3f31ff36c8557e35cc322
Dustin Carmelo <dustcarmelo@yahoo.com>
2025-02-18 18:49:48 -0500

    refactored effect actor to apply multiple effects (if set), implemented application and removal of infinite effects, created a fire area and effect, reorganized effect actors

--- Source/AuraGAS/Private/Actor/AGASEffectActor.cpp
+++ Source/AuraGAS/Private/Actor/AGASEffectActor.cpp
@@ -20,18 +20,107 @@ void AAGASEffectActor::BeginPlay()
 
 }
 
-void AAGASEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+void AAGASEffectActor::ApplyEffectToTarget(AActor* TargetActor, const TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
-	UAbilitySystemComponent* TargetAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	UAbilitySystemComponent* TargetAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
 	if (TargetAbilitySystemComponent == nullptr) return;
 
 	check(GameplayEffectClass);
 	
-	// note: when it comes to handle variable names, other projects generally omits the "handle" portion of the name
+	// note: when it comes to handle variable names, other projects generally omit the "handle" portion of the name
 	FGameplayEffectContextHandle EffectContextHandle = TargetAbilitySystemComponent->MakeEffectContext();
 	EffectContextHandle.AddSourceObject(this);
 	
 	const FGameplayEffectSpecHandle EffectSpecHandle = TargetAbilitySystemComponent->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
-	TargetAbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data);
+	const FActiveGameplayEffectHandle ActiveEffectHandle = TargetAbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data);
+
+	const bool bIsInfinite = EffectSpecHandle.Data->Def->DurationPolicy == EGameplayEffectDurationType::Infinite;
+	if (bIsInfinite && InfiniteEffectRemovalPolicy ==  EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		auto& AffectedActor = ActiveInfiniteEffects.FindOrAdd(TargetActor->GetUniqueID());
+		auto& InstigatorActorArray = AffectedActor.FindOrAdd(GetUniqueID());
+		InstigatorActorArray.Add(ActiveEffectHandle);
+	}
+
+	if (bDestroyOnEffectApplication)
+	{
+		Destroy();
+	}
+}
+
+void AAGASEffectActor::OnOverlap(AActor* TargetActor)
+{
+	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		for (const auto& InstantEffect : InstantGameplayEffectClasses)
+		{
+			ApplyEffectToTarget(TargetActor, InstantEffect);
+		}
+	}
+	if (DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		for (const auto& DurationEffect : DurationGameplayEffectClasses)
+		{
+			ApplyEffectToTarget(TargetActor, DurationEffect);
+		}
+	}
+	if (InfiniteEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		for (const auto& InfiniteEffect : InfiniteGameplayEffectClasses)
+		{
+			ApplyEffectToTarget(TargetActor, InfiniteEffect);
+		}
+	}
+}
+
+void AAGASEffectActor::RemoveInfiniteEffectsFromActor(AActor* TargetActor)
+{
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (!IsValid(TargetASC)) return;
+		
+	const auto UID = TargetActor->GetUniqueID();
+	if (!ActiveInfiniteEffects.Contains(UID)) return;
+	if (!ActiveInfiniteEffects[UID].Contains(GetUniqueID())) return;
+
+	auto& ActorsAppliedEffects = ActiveInfiniteEffects[UID][GetUniqueID()];
+	for (const auto& AppliedEffect : ActorsAppliedEffects)
+	{
+		if (!TargetASC->RemoveActiveGameplayEffect(AppliedEffect, 1)) return;
+	}
+
+	ActiveInfiniteEffects[UID].Remove(GetUniqueID());
+	if (ActiveInfiniteEffects[UID].IsEmpty())
+	{
+		ActiveInfiniteEffects.Remove(UID);
+	}
+}
+
+void AAGASEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		for (const auto& InstantEffect : InstantGameplayEffectClasses)
+		{
+			ApplyEffectToTarget(TargetActor, InstantEffect);
+		}
+	}
+	if (DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		for (const auto& DurationEffect : DurationGameplayEffectClasses)
+		{
+			ApplyEffectToTarget(TargetActor, DurationEffect);
+		}
+	}
+	if (InfiniteEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		for (const auto& InfiniteEffect : InfiniteGameplayEffectClasses)
+		{
+			ApplyEffectToTarget(TargetActor, InfiniteEffect);
+		}
+	}
+	if (InfiniteEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		RemoveInfiniteEffectsFromActor(TargetActor);
+	}
 }
 
--- Source/AuraGAS/Public/Actor/AGASEffectActor.h
+++ Source/AuraGAS/Public/Actor/AGASEffectActor.h
@@ -3,10 +3,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ActiveGameplayEffectHandle.h"
 #include "GameFramework/Actor.h"
 #include "AGASEffectActor.generated.h"
 
+class UAbilitySystemComponent;
 class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy : uint8
+{
+	ApplyOnOverlap,
+	ApplyOnEndOverlap,
+	DoNotApply
+};
+
+UENUM(BlueprintType)
+enum class EEffectRemovalPolicy : uint8
+{
+	RemoveOnEndOverlap,
+	DoNotRemove
+};
 
 UCLASS()
 class AURAGAS_API AAGASEffectActor : public AActor
@@ -20,9 +37,55 @@ protected:
 	virtual void BeginPlay() override;
 
 	UFUNCTION(BlueprintCallable)
-	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	void ApplyEffectToTarget(AActor* TargetActor, const TSubclassOf<UGameplayEffect> GameplayEffectClass);
 	
-	UPROPERTY(EditAnywhere, Category = "AppliedEffects")
-	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+	UFUNCTION(BlueprintCallable)
+	void OnOverlap(AActor* TargetActor);
 	
+	void RemoveInfiniteEffectsFromActor(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable)
+	void OnEndOverlap(AActor* TargetActor);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AppliedEffects")
+	bool bDestroyOnEffectApplication = false;
+	
+	//~ GameplayEffect Types and Policies Begin
+	// Instant Effect
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AppliedEffects")
+	TArray<TSubclassOf<UGameplayEffect>> InstantGameplayEffectClasses;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AppliedEffects")
+	EEffectApplicationPolicy InstantEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	// Duration Effect
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AppliedEffects")
+	TArray<TSubclassOf<UGameplayEffect>> DurationGameplayEffectClasses;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AppliedEffects")
+	EEffectApplicationPolicy DurationEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	// Infinite Effect
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AppliedEffects")
+	TArray<TSubclassOf<UGameplayEffect>> InfiniteGameplayEffectClasses;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AppliedEffects")
+	EEffectApplicationPolicy InfiniteEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AppliedEffects")
+	EEffectRemovalPolicy InfiniteEffectRemovalPolicy = EEffectRemovalPolicy::RemoveOnEndOverlap;
+	//~ GameplayEffect Types and Policies End
+
+	/**
+	 * Map that holds the TargetActor's UID as key and has a value of another map which holds the actors
+	 * that apply the effects and what effects they apply.
+	 *
+	 * main map - key: TargetActor UID, value: Map of effect actors and their applied effects
+	 * value map - key: EffectActor UID, value: Array of applied effects
+	 *
+	 * Tbh, this seems a little overkill for knowing what gameplay effects one actor is applying, but I can't
+	 * think of anything else at the moment
+	 */
+	TMap<uint32, TMap<uint32, TArray<FActiveGameplayEffectHandle>>> ActiveInfiniteEffects;
+
 };
